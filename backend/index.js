5eb8e52afcbd3b02a77dc25f7f3c1d6a5d7c1e06
Arman Alimov <527c@mail.ru>
2022-01-25 18:35:14 +0600

    router

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,15 @@ const models = require('./models/models')
 
 const cors = require('cors')
 
+const router = require('./routes/index')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api/1.0', router)
+
 
 const start = async () => {
     try {
