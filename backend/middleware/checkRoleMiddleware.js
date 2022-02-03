bf4304d6a295875892f679be77c22fee79dbea42
Arman Alimov <527c@mail.ru>
2022-02-03 23:33:58 +0600

    checkRoleMiddleware done

--- backend/middleware/checkRoleMiddleware.js
+++ backend/middleware/checkRoleMiddleware.js
@@ -11,6 +11,9 @@ module.exports = function (role) {
                 return res.status(401).json({message: "Не авторизован!"})
             }
             const decoded = jwt.verify(token, process.env.SECRET_KEY)
+            if (decoded.role !== role) {
+                return res.status(403).json({message: "Нет доступа!"})
+            }
             req.user = decoded
             next()
 
